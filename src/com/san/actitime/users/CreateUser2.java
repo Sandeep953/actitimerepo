892a472dc46732f999d9156456f61e13d909c631
Sandeep953 <sandeep953.sp@gmail.com>
2024-06-26 20:18:25 +0530

    Committing createUser2 file to BranchX

---
+++ src/com/san/actitime/users/CreateUser2.java
@@ -0,0 +1,12 @@
+package com.san.actitime.users;
+
+import org.testng.annotations.Test;
+
+import com.san.actitime.testbase.Baseclass;
+
+public class CreateUser2 extends Baseclass {
+  @Test
+  public void testcreateuser() {
+	  System.out.println("User Created");
+  }
+}
